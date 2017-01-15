c814de6f68e1476ef405c95cc045601510ed1d3f
Sean Abrahams <abrahams@gmail.com>
2017-01-15 15:44:13 -0800

    Fix function arguments.

--- lib/lastfm/track.ex
+++ lib/lastfm/track.ex
@@ -1,15 +1,20 @@
 defmodule Lastfm.Track do
   use Lastfm.Base
 
-  def get_similar(args \\ [autocorrect: 1, limit: 30])
-  def get_similar([mbid: mbid] = args) do
-    get!("?method=track.getsimilar&mbid=#{args[:mbid]}&autocorrect=#{args[:autocorrect]}limit=#{args[:limit]}").body
-  end
-  def get_similar([track: track, artist: artist] = args) do
-    get!("?method=track.getsimilar&artist=#{args[:artist]}&track=#{args[:track]}&autocorrect=#{args[:autocorrect]}limit=#{args[:limit]}").body
+  def get_similar(options \\ []) do
+    mbid = Keyword.get(options, :mbid, nil)
+    artist = Keyword.get(options, :artist, nil)
+    track = Keyword.get(options, :track, nil)
+    autocorrect = Keyword.get(options, :autocorrect, 1)
+    limit = Keyword.get(options, :limit, 30)
+    if mbid do
+      get!("?method=track.getsimilar&mbid=#{mbid}&autocorrect=#{autocorrect}limit=#{limit}").body
+    else
+      get!("?method=track.getsimilar&artist=#{artist}&track=#{track}&autocorrect=#{autocorrect}limit=#{limit}").body
+    end
   end
 
-  def search(query, args \\ [page: 1, limit: 30]) do
-    get!("?method=track.search&track=#{query}&artist=#{args[:artist]}&page=#{args[:page]}&limit=#{args[:limit]}").body
+  def search(query, options \\ [page: 1, limit: 30]) do
+    get!("?method=track.search&track=#{query}&artist=#{options[:artist]}&page=#{options[:page]}&limit=#{options[:limit]}").body
   end
 end
